fdebc556633d44c45fb5cc10e1ea614d0b739618
Yurii Tsimura <chappa2828@gmail.com>
2021-12-21 15:54:11 +0200

    basis of structure

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+{
+  /* <ul class="friend-list">
+  <!-- Произвольное кол-во FriendListItem -->
+</ul> */
+}
+
+{
+  /* <li class="item">
+  <span class="status"></span>
+  <img class="avatar" src="" alt="User avatar" width="48" />
+  <p class="name"></p>
+</li> */
+}
