2b7c2c9b7a3902cc88ae9df448b0e9719d7de206
Yurii Tsimura <chappa2828@gmail.com>
2021-12-22 11:55:25 +0200

    removed unnecessary

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,33 +1,17 @@
 import PropTypes from 'prop-types';
 import StatisticsSection from '../StatisticsSection/StatisticsSection';
-// import StatisticsList from 'components/StatisticsList/StatisticsList';
-// import StatisticsTitle from '../StatisticsTitle/StatisticsTitle';
 import data from './data.json';
-
-// https://github.com/goitacademy/react-homework/tree/master/homework-01
 
 const Statistics = () => {
   return (
     <>
       <StatisticsSection data={data} />
-      {/* <StatisticsTitle /> */}
-      {/* <StatisticsList data={data} /> */}
     </>
   );
 };
 
-// const Statistics = ({ label, percentage }) => {
-//   return (
-//     <li className="item">
-//       <span className="label">{label}</span>
-//       <span className="percentage">{percentage + '%'}</span>
-//     </li>
-//   );
-// };
 Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.object,
 };
 export default Statistics;
-
-// https://youtu.be/kfX5HTQD26E?t=5050
--- src/components/StatisticsTitle/StatisticsTitle.jsx
+++ src/components/StatisticsTitle/StatisticsTitle.jsx
@@ -2,5 +2,3 @@ const StatisticsTitle = () => {
   return <h2 className="title">Upload stats</h2>;
 };
 export default StatisticsTitle;
-
-// https://youtu.be/lyrS0NqmmF8?t=3695
